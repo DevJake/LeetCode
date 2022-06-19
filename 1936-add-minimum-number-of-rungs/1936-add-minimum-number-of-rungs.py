899e76f69ca86584d8961ddb482ac094f35ba0fe
Jake <DevJake@users.noreply.github.com>
2022-06-19 20:18:49 +0100

    Time: 577 ms (90.43%), Space: 28.1 MB (61.17%) - LeetHub

--- 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.py
+++ 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.py
@@ -1,3 +1,3 @@
 class Solution:
     def addRungs(self, rungs: List[int], dist: int) -> int:
-        return sum([(rungs[0]-1)//dist] + [(r1-r0-1)//dist for r0, r1 in zip(rungs, rungs[1:]) if r1-r0 > dist])
+        return sum([(r1-r0-1)//dist for r0, r1 in zip([0] + rungs, rungs) if r1-r0 > dist])
