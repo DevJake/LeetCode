f65ed409f0ce2c6faa7e4eca673ce6b2d0d41365
Jake <DevJake@users.noreply.github.com>
2022-06-19 13:04:14 +0100

    Time: 1152 ms (6.00%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,2 +1,7 @@
 # Write your MySQL query statement below
-SELECT employee_id, IF(SUBSTR(name, 1, 1) != 'M' AND employee_id % 2 = 1, salary, 0) AS bonus FROM Employees ORDER BY employee_id
+SELECT employee_id, 
+    IF(SUBSTR(name, 1, 1) != 'M' 
+    AND employee_id % 2 = 1, salary, 0) 
+    AS bonus 
+FROM Employees 
+ORDER BY employee_id;
