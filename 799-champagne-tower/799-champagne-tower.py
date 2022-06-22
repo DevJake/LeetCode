54fc1132a57c6bebf29cff523633bcb6843455eb
Jake <DevJake@users.noreply.github.com>
2022-06-22 17:23:43 +0100

    Time: 313 ms (16.49%), Space: 14 MB (62.22%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.py
+++ 799-champagne-tower/799-champagne-tower.py
@@ -7,13 +7,10 @@ class Solution:
             n_row = [0] * (len(c_row)+1)
             
             for j in range(len(c_row)):
-                fill_amount = min(1, c_row[j])
-                overflow_amount = max(0, c_row[j] - 1)
-                n_row[j] = n_row[j] + (overflow_amount / 2)
+                n_row[j] = n_row[j] + (max(0, c_row[j] - 1) / 2)
+                n_row[j+1] = n_row[j+1] + (max(0, c_row[j] - 1) / 2)
                 
-                n_row[j+1] = n_row[j+1] + (overflow_amount / 2)
-                
-                c_row[j] = fill_amount
+                c_row[j] = min(1, c_row[j])
                 
             tower.append(n_row)
         
