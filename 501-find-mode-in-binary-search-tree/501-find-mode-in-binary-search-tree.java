7f718636cb8124a7cfdbb10cb274754fa0ee1d1d
Jake <DevJake@users.noreply.github.com>
2022-06-23 23:56:38 +0100

    Time: 15 ms (12.65%), Space: 44.5 MB (81.66%) - LeetHub

---
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private TreeMap<Integer, Integer> map = new TreeMap<>();
+    
+    public int[] findMode(TreeNode root) {
+        postorder(root);
+        
+        // int largestMode = map.lastEntry().getValue();
+        int largestMode = Collections.max(map.values());
+        
+        return map.entrySet().stream().filter(e -> e.getValue() == largestMode).mapToInt(v -> v.getKey()).toArray();
+    }
+    
+    void postorder(TreeNode node){
+        if (node.left != null)
+            postorder(node.left);
+        
+        if (node.right != null)
+            postorder(node.right);
+        
+        map.put(node.val, map.getOrDefault(node.val, 0) + 1);
+    }
+}
