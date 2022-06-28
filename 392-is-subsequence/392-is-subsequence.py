f7174e62af9305609cd5ef4dea79723db5209179
Jake <DevJake@users.noreply.github.com>
2022-06-29 00:31:28 +0100

    Time: 50 ms (52.34%), Space: 14 MB (44.71%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.py
@@ -0,0 +1,11 @@
+import re
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        for c in s:
+            if c not in t:
+                return False
+            
+            t = t[t.index(c)+1:]
+            print(t)
+        return True
