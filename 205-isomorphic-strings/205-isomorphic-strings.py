379d4219e97522d8c41dc90f9d6388b865e03594
Jake <DevJake@users.noreply.github.com>
2022-06-29 00:03:35 +0100

    Time: 41 ms (92.95%), Space: 14.1 MB (88.48%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        mapping = {}
+        
+        for i, l in enumerate(s):            
+            if l not in mapping and t[i] not in mapping.values():
+                mapping[l] = t[i]
+                
+            if mapping.get(l, '') != t[i]:
+                return False
+                
+        return True
