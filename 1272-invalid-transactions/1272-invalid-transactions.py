ceadb019eb5385d651ab5bf8c88e825f7c77485c
Jake <devjake.github-contact@privatemail.ink>
2023-11-02 12:00:51 +0000

    Time: 98 ms (42.35%), Space: 17.1 MB (37.16%) - LeetHub

--- 1272-invalid-transactions/1272-invalid-transactions.py
+++ 1272-invalid-transactions/1272-invalid-transactions.py
@@ -1,9 +1,10 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        # t[i] = name,time,amount,city
+        # t[i] = "name,time,amount,city"
 
         seen = {}
         invalid = set()
+
         for i, t in enumerate(transactions):
             name, time, amount, city = t.split(',')
             time, amount = int(time), int(amount)
@@ -17,7 +18,7 @@ class Solution:
             past = seen[name]
             
             for t0 in seen[name]:
-                time0, city0, amount0, index0, s = t0
+                time0, city0, amount0, i0 = t0
                 if t0 == t:
                     continue
 
@@ -26,8 +27,8 @@ class Solution:
 
                 if city0 != city and abs(time0-time) <= 60:
                     invalid.add(i)
-                    invalid.add(index0)
+                    invalid.add(i0)
                 
 
-            seen[name].append((time, city, amount, i, t))
+            seen[name].append((time, city, amount, i))
         return [transactions[i] for i in invalid]
