eeac94208b0b4cb46a7ed0c29886c9fa95b911da
Jake <devjake.github-contact@privatemail.ink>
2024-04-03 15:45:16 +0100

    Time: 40 ms (18.39%) | Memory: 16.5 MB (61.04%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,17 +1,13 @@
 class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
+    def wordPattern(self, p: str, s: str) -> bool:
         m = {}
-        seen = set()
 
-        if len(list(pattern)) != len(s.split(' ')):
-            return False
+        if len(list(p)) != len(s.split(' ')): return False
+        if len(set(p)) != len(set(s.split(' '))): return False
         
-        for p, a in zip(list(pattern), s.split(' ')):
+        for p, a in zip(list(p), s.split(' ')):
             if p not in m:
-                if a in seen:
-                    return False
                 m[p] = a
-                seen.add(a)
             
             if m[p] != a:
                 return False
